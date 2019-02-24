46c8d7eddefe08afc2fe6f42e30799e8e8469ba0
benhodge <benjamin.andrew.hodge@gmail.com>
2019-02-24 14:49:46 -0600

    added accounts-user

--- git_graphed_api/lib/git_graphed_api_web/router.ex
+++ git_graphed_api/lib/git_graphed_api_web/router.ex
@@ -2,10 +2,10 @@ defmodule GitGraphedApiWeb.Router do
   use GitGraphedApiWeb, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/api", GitGraphedApiWeb do
-    pipe_through :api
+    pipe_through(:api)
   end
 end
