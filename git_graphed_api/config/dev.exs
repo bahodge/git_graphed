35078ee9a85eaa1c3aeefa8b141d57cdaff8e7cd
benhodge <benjamin.andrew.hodge@gmail.com>
2019-02-25 18:29:51 -0600

    updated config to use secret exs on dev

--- git_graphed_api/config/dev.exs
+++ git_graphed_api/config/dev.exs
@@ -47,10 +47,13 @@ config :phoenix, :stacktrace_depth, 20
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
 
-# Configure your database
-config :git_graphed_api, GitGraphedApi.Repo,
-  username: "benjaminhodge",
-  password: "qwerty2017!",
-  database: "git_graphed_api_dev",
-  hostname: "localhost",
-  pool_size: 10
+# Configure your database # EXAMPLE!
+# config :git_graphed_api, GitGraphedApi.Repo,
+#   username: "your_username_for_your_local_database",
+#   password: "your_password_for_your_local_database",
+#   database: "git_graphed_api_dev",
+#   hostname: "localhost",
+#   pool_size: 10
+
+# create a local 'dev.secret.exs' file to hold your secret db config
+import_config "dev.secret.exs"
