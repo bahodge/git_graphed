18a33e860e79793a92a382e7052ce42c0777e126
benhodge <benjamin.andrew.hodge@gmail.com>
2019-03-09 15:04:53 -0600

    updating frontend

--- git_graphed_frontend/src/config/absinthe-socket-link.js
+++ git_graphed_frontend/src/config/absinthe-socket-link.js
@@ -3,5 +3,8 @@
 import * as AbsintheSocket from '@absinthe/socket';
 import { createAbsintheSocketLink } from '@absinthe/socket-apollo-link';
 import { Socket as PhoenixSocket } from 'phoenix';
+import Cookies from 'js-cookie';
 
-export default createAbsintheSocketLink(AbsintheSocket.create(new PhoenixSocket('ws://localhost:4000/socket')));
+export default createAbsintheSocketLink(
+	AbsintheSocket.create(new PhoenixSocket('ws://localhost:4000/socket', { params: { token: 'BEN IS GREAT!' } }))
+);
